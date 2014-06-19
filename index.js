b9cefd498e0c06f58c8eac5cfd3c4cb2654c005a
Florent Vilmart <vfloz@me.com>
2014-06-19 11:18:35 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var Parse = require("parse").Parse;
+
+Parse.Cloud = {};
+Parse.Cloud.job = Parse.Cloud.define = Parse.Cloud.beforeSave =
+Parse.Cloud.afterSave = Parse.Cloud.beforeDelete = Parse.Cloud.afterDelete = function(){};
+Parse.Cloud.httpRequest = require("./lib/httpRequest");
+
+Parse.Cloud._expressCookieEncryptionKey = function(){
+	return "34dc3e303f57bd18f4b1d16995d36245";
+}
+Parse.User.prototype.getSessionToken = function(){
+    return this._sessionToken;
+}
+
+module.exports.Parse = Parse;
