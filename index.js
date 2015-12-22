47084056b4c3d1864b7cb8a0d312ea260f462092
Florent Vilmart <florentvilmart@me.com>
2015-12-22 18:52:16 +0100

    Replace implementation of Parse.Cloud.httpRequest

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var bodyParser = require('body-parser');
 
 var registered = {}
 
-//Parse.Cloud.httpRequest = require("./lib/httpRequest");
-
 Parse.Cloud._expressCookieEncryptionKey = function(){
 	return "34dc3e303f57bd18f4b1d16995d36245";
 }
+
+// Override httpRequest here as the parse-cloud-express verison don't work!
+Parse.Cloud.httpRequest = require("./lib/httpRequest");
 
 module.exports.Parse = Parse;
 module.exports.ParseCloud = ParseCloud;
